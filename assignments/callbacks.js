b2253d5f0f1ed99726a787fb3901270c74823241
Andrew Brudnak <brudnak@icloud.com>
2019-05-07 13:12:44 -0700

    array-methods complete +
    - Working on callbacks.js
    
    - Experimenting with JavaScript
    meta programming and using objects
    define property to make the following
    
    function last(arr, cb) {
      return cb(arr.last)
    }
    last(items, e => console.log(e))
    
    - I think return cb(arr.last) looks more readable
    and could be useful for code that
    runs multiple places, and not just once

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -1,3 +1,7 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Meta_programming
+Object.defineProperty(Array.prototype, 'last', {
+  get: function() { return this[this.length - 1] }
+})
 // Create a higher order function and invoke the callback function to test your work. You have been provided an example of a problem and a solution to see how this works with our items array.  Study both the problem and the solution to figure out the rest of the problems.
 
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
@@ -26,25 +30,30 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
 function getLength(arr, cb) {
-  // getLength passes the length of the array into the callback.
+  return cb(arr.length)
 }
+getLength(items, e => console.log(e))
 
+// Special
 function last(arr, cb) {
-  // last passes the last item of the array into the callback.
+  return cb(arr.last)
 }
+last(items, e => console.log(e))
 
 function sumNums(x, y, cb) {
-  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y)
 }
+sumNums(18, 1, e => console.log(e))
 
 function multiplyNums(x, y, cb) {
-  // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y)
 }
+multiplyNums(2, 6, e => console.log(e))
 
 function contains(item, list, cb) {
-  // contains checks if an item is present inside of the given array/list.
-  // Pass true to the callback if it is, otherwise pass false.
+  return cb(list.includes(item))
 }
+contains('Notebook', items, e => console.log(e))
 
 /* STRETCH PROBLEM */
 
